049643d86edc54aa383f316950426beb24b0ad8b
fanbc <fanbc@labsys.cn>
2019-06-04 15:11:52 +0800

    102

--- src/Migration/2019_01_14_100004_create_admin_user_table.php
+++ src/Migration/2019_01_14_100004_create_admin_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminTable extends Migration
+class CreateAdminUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,9 +17,9 @@ class CreateAdminTable extends Migration
         Schema::create($tableName, function (Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->string('name',30)->unique()->comment('姓名');
-            $table->string('password',50)->comment('密码');
+            $table->string('password',70)->comment('密码');
             $table->string('avatar',100)->comment('头像');
-            $table->string('email',50)->unique()->comment('邮箱');
+            $table->string('email',50)->comment('邮箱');
             $table->tinyInteger('status')->default(1)->comment('1正常2禁用');
             $table->string('remember_token',100)->comment('记住我');
             $table->timestamps();
