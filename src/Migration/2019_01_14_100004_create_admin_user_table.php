890054f8e660a20a157ea0cc3cb5bb9175b0ff12
fanbc <fanbc@labsys.cn>
2020-04-10 10:26:56 +0800

    15

--- src/Migration/2019_01_14_100004_create_admin_user_table.php
+++ src/Migration/2019_01_14_100004_create_admin_user_table.php
@@ -14,16 +14,18 @@ class CreateAdminUserTable extends Migration
     public function up()
     {
         $tableName = Config::get('auth_gaia.users_table');
-        Schema::create($tableName, function (Blueprint $table) {
-            $table->increments('id')->unsigned();
-            $table->string('name',30)->unique()->comment('姓名');
-            $table->string('password',70)->comment('密码');
-            $table->string('avatar',100)->comment('头像');
-            $table->string('email',50)->comment('邮箱');
-            $table->tinyInteger('status')->default(1)->comment('1正常2禁用');
-            $table->string('remember_token',100)->comment('记住我');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable($tableName)){
+            Schema::create($tableName, function (Blueprint $table) {
+                $table->increments('id')->unsigned();
+                $table->string('name',30)->unique()->comment('姓名');
+                $table->string('password',70)->comment('密码');
+                $table->string('avatar',100)->comment('头像');
+                $table->string('email',50)->comment('邮箱');
+                $table->tinyInteger('status')->default(1)->comment('1正常2禁用');
+                $table->string('remember_token',100)->comment('记住我');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
